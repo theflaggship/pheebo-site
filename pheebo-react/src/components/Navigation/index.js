48eef3f954ee0b46ebe7e61e9d63ab6aadc5385f
Owen Iwamasa <owiwamasa@gmail.com>
2021-09-16 13:00:45 -0400

    changed logo and background color

--- pheebo-react/src/components/Navigation/index.js
+++ pheebo-react/src/components/Navigation/index.js
@@ -6,7 +6,7 @@ function Navigation() {
     return (
         <div className='navbar'>
             <div className='nav-logo-container'>
-                <img src='https://i.imgur.com/r1zPmsN.png' className='nav-logo' alt='pheebo logo' />
+                <img src='https://i.imgur.com/ECiuoDw.png' className='nav-logo' alt='pheebo logo' />
             </div>
             <div className='nav-links'>
                 <NavLink to='/' className='nav-home'>Home</NavLink>
